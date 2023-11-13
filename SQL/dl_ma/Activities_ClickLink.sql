ee5904d8cd9ef7b01f42023c75f68570f496a883
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-13 19:46:42 +0900

    Create Activities_ClickLink.sql

---
+++ SQL/dl_ma/Activities_ClickLink.sql
@@ -0,0 +1,11 @@
+CREATE TABLE							
+	dl_ma.Activities_ClickLink
+(							
+	ActivityId		STRING,
+	ActivityDate	TIMESTAMP,
+	LeadId			STRING,
+	LinkID			INT64,
+	LinkIDValue		STRING,
+	QueryParameters	STRING,
+	ReferrerURL		STRING
+)	
