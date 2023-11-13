2478a44e0117423f707dce8e92edea5c094fd10a
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-13 12:58:15 +0900

    Create lifecycle_status.sql

---
+++ SQL/function/lifecycle_status.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION `pi-dashboard-398109.datamart.lifecycle_status`(lifecycle_status STRING) RETURNS STRING AS (
+(
+    SELECT
+      CASE
+        WHEN lifecycle_status IN ('MCL') THEN 'MCL'
+        WHEN lifecycle_status IN ('MEL','MEL1','MEL2') THEN 'MEL'
+        WHEN lifecycle_status IN ('MQL') THEN 'MQL'
+        WHEN lifecycle_status IN ('SAL') THEN 'SAL'
+        WHEN lifecycle_status IN ('SQL','SQL1','SQL2') THEN 'SQL'
+        ELSE lifecycle_status
+      END
+  )
+);
