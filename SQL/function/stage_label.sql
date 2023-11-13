949e0fcf258923ff2bb4fef91921170b86bce9f3
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-13 12:58:55 +0900

    Create stage_label.sql

---
+++ SQL/function/stage_label.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION `pi-dashboard-398109.datamart.stage_label`(ForecastCategory STRING) RETURNS STRING AS (
+(
+    SELECT
+      CASE
+        WHEN ForecastCategory = "Won" THEN '1.実績'
+        WHEN ForecastCategory = "Closed" THEN '1.実績'
+        WHEN ForecastCategory = "Commit" THEN '2.確実'
+        WHEN ForecastCategory = "Forecast" THEN '2.確実'
+        WHEN ForecastCategory = "Best Case" THEN '3.見込'
+        WHEN ForecastCategory = "BestCase" THEN '3.見込'
+        WHEN ForecastCategory = "Pipeline" THEN '3.見込'
+        WHEN ForecastCategory = "Pileline" THEN '3.見込'
+        WHEN ForecastCategory = "Upside" THEN '3.見込'
+        ELSE 'その他'
+      END
+  )
+);
