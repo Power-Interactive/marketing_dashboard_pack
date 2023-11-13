cd7f82391b4999b0bd8fade19ca15a7b127539c4
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-13 19:46:55 +0900

    Create Activities_FillOutForm.sql

---
+++ SQL/dl_ma/Activities_FillOutForm.sql
@@ -0,0 +1,11 @@
+CREATE TABLE							
+	dl_ma.Activities_FillOutForm
+(							
+	ActivityId		STRING,
+	ActivityDate	TIMESTAMP,
+	LeadId			STRING,
+	ProgramName		STRING,
+	ProgramType		STRING,
+	WebformID		INT64,
+	WebformIDValue	STRING
+)	
