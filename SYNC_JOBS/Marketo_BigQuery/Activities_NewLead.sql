cfc60ddbfe245027c22bb0b2142ddd4fd4e2d0c8
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-13 20:00:48 +0900

    Create Activities_NewLead.sql

---
+++ SYNC_JOBS/Marketo_BigQuery/Activities_NewLead.sql
@@ -0,0 +1,1 @@
+CACHE [Activities_NewLead] SELECT * FROM [Activities_NewLead] WHERE [ActivityDate] >= CONCAT(SUBSTRING(DATEADD('d', -1, CURRENT_DATE()), 0, 10), ' 15:00:00') AND [ActivityDate] < CONCAT(SUBSTRING(CURRENT_DATE(), 0, 10), ' 15:00:00')
