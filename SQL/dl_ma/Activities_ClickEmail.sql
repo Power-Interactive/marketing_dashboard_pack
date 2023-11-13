026791f73a63efea5d0af369fab800f1ee5e9edc
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-13 19:46:22 +0900

    Create Activities_ClickEmail.sql

---
+++ SQL/dl_ma/Activities_ClickEmail.sql
@@ -0,0 +1,10 @@
+CREATE TABLE							
+	dl_ma.Activities_ClickEmail
+(							
+	ActivityId		STRING,
+	ActivityDate	TIMESTAMP,
+	LeadId			STRING,
+	MailingID		INT64,
+	MailingIDValue	STRING,
+	Link			STRING
+)
