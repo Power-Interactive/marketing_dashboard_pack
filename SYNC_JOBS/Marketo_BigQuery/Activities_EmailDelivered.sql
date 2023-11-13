d60ef8594e138d2080d6b8438b51c14a0bb923a8
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-13 20:00:21 +0900

    Create Activities_EmailDelivered.sql

---
+++ SYNC_JOBS/Marketo_BigQuery/Activities_EmailDelivered.sql
@@ -0,0 +1,1 @@
+CACHE [Activities_EmailDelivered] SELECT [ActivityId], [ActivityDate], [CampaignId], [LeadId], [MailingID], [MailingIDValue] FROM [Activities_EmailDelivered] WHERE [ActivityDate] >= CONCAT(SUBSTRING(DATEADD('d', -1, CURRENT_DATE()), 0, 10), ' 15:00:00') AND [ActivityDate] < CONCAT(SUBSTRING(CURRENT_DATE(), 0, 10), ' 15:00:00')
