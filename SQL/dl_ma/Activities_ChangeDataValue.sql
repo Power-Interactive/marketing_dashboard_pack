090801ff76b495be44dd68de76685aebe26bd4e4
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-13 19:45:58 +0900

    Create Activities_ChangeDataValue.sql

---
+++ SQL/dl_ma/Activities_ChangeDataValue.sql
@@ -0,0 +1,13 @@
+CREATE TABLE							
+	dl_ma.Activities_ChangeDataValue
+(							
+	ActivityId			STRING,
+	ActivityDate		TIMESTAMP,
+	AttributeName		INT64,
+	AttributeNameValue	STRING,
+	CampaignId			STRING,
+	LeadId				STRING,
+	NewValue			STRING,
+	OldValue			STRING,
+	Reason				STRING
+)	
