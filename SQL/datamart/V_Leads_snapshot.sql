85ef7c5799f7fe2eec7371ad66d2798bbe36fc7c
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-20 16:40:22 +0900

    Update V_Leads_snapshot.sql

--- SQL/datamart/V_Leads_snapshot.sql
+++ SQL/datamart/V_Leads_snapshot.sql
@@ -1,26 +1,29 @@
+--W_Leads_snapdate の更新が必要
 WITH LatestActivity AS (
     SELECT 
         L.snapshot_date,
         L.customer_id,
         L.createdAt,
-        L.LeadSource1 AS LeadSource1,
+        datamart.lead_source(LeadSource1) AS lead_source,
         L.lifecycle_status AS OriginalLifecycleStatus,
         AC.NewValue,
         ROW_NUMBER() OVER(PARTITION BY L.customer_id, L.snapshot_date ORDER BY AC.ActivityDate DESC) AS row_num
     FROM
-        `pi-dev-dashboard.dev_.Leads_snapdate` AS L
+        `pi-dev-dashboard.datamart.W_Leads_snapdate` AS L
     LEFT JOIN
-        `pi-dev-dashboard.sample_ma.Activities_ChangeDataValue` AS AC ON L.customer_id = AC.LeadId AND DATE(AC.ActivityDate, "Asia/Tokyo") <= L.snapshot_date
+        `pi-dev-dashboard.datamart.V_Lead_ChangeDataValue` AS AC ON L.customer_id = AC.LeadId AND CAST(SUBSTR(AC.ActivityDate, 1, 10) AS DATE) <= L.snapshot_date
     WHERE
-        L.snapshot_date >= L.createdAt
+        L.snapshot_date >= DATE(L.createdAt, "Asia/Tokyo")
+        
 )
 
 SELECT
     snapshot_date,
     customer_id,
-    createdAt,
-    LeadSource1,   
-    COALESCE(NewValue, OriginalLifecycleStatus) AS lifecycle_status
+    createdAt AS CreatedAt,
+    lead_source,   
+    COALESCE(NewValue, OriginalLifecycleStatus) AS lifecycle_status,
+    CONCAT(EXTRACT(YEAR FROM  DATE (snapshot_date)),"年",EXTRACT(month FROM  DATE (snapshot_date)),"月") year_month,
 FROM
     LatestActivity
 WHERE
