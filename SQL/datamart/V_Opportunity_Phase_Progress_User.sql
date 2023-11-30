bd00ea577e5232794629f25500daec82986ca708
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:49:01 +0900

    Update V_Opportunity_Phase_Progress_User.sql

--- SQL/datamart/V_Opportunity_Phase_Progress_User.sql
+++ SQL/datamart/V_Opportunity_Phase_Progress_User.sql
@@ -4,23 +4,17 @@ AS
 (
 SELECT
   DATE_TRUNC(t1.close_date, MONTH) month
- ,t1.contact_status
- ,datamart.ForecastCategory(t1.forecast_phase) AS forecast_phase --永続関数を参照
+ ,datamart.forecast_category(t1.forecast_phase) AS forecast_category --永続関数を参照
  ,datamart.stage_label(t1.forecast_phase) AS stage_label --永続関数を参照
  ,t2.department
  ,t2.office
- ,CONCAT(t2.last_name,t2.first_name) user
+ ,CONCAT(t2.last_name,t2.first_name) AS user
  ,SUM(t1.amount) amount
-FROM `pi-dashboard-398109.datawarehouse.Opportunity`  t1
-LEFT JOIN `pi-dashboard-398109.datawarehouse.User` t2
+FROM `pi-dev-dashboard.datawarehouse.Opportunity`  t1
+LEFT JOIN `pi-dev-dashboard.datawarehouse.User` t2
  ON t1.owner_id = t2.Id
 GROUP BY 
-  DATE_TRUNC(t1.close_date, MONTH)
- ,t1.contact_status
- ,t1.forecast_phase
- ,t2.department
- ,t2.office
- ,CONCAT(t2.last_name,t2.first_name) 
+  1,2,3,4,5,6
 ),
 
 T1_tmp
@@ -31,55 +25,47 @@ SELECT
   department,
   office,
   user,
-  ifnull(sum(amount),0) AS amount
+  ifnull(sum(Amount),0) AS amount
 FROM
   T1
 GROUP BY
-  month,
-  user,
-  department,
-  office
+  1,2,3,4
 ),
 
 T2
 AS
 (
 SELECT
-  S1.month,
-  '4.過不足' stage_name,
+  S1.Month AS month,
   '4.過不足' stage_label,
-  S1.office,
-  S1.department,
-  S1.user AS user,
-  ifnull(sum(S1.sales_target),0)-ifnull(sum(T1.amount),0) AS amount,
+  S1.Office AS office,
+  S1.Department AS department,
+  S1.User AS user,
+  ifnull(sum(S1.SalesTarget),0)-ifnull(sum(T1.Amount),0) AS amount,
 FROM
-  `pi-dashboard-398109.datawarehouse.Sales_Target_User_Master` AS S1
+  `pi-dev-dashboard.master.SalesTarget_User` AS S1
 LEFT JOIN
   T1_tmp AS T1
 ON
-  S1.month = T1.month
-	AND S1.User = T1.User
-  AND S1.office = T1.office
-  AND S1.department = T1.department
+  S1.Month = T1.month
+  AND S1.Office = T1.office
+  AND S1.Department = T1.department
+  AND S1.User = T1.user
 GROUP BY
-  S1.month,
-  S1.user,
-  S1.office,
-  S1.department
+  1,2,3,4,5
 )
 
 
 SELECT
   month,
-  datamart.fiscal_year(Month) AS fiscal_year, --永続関数を参照
-  CONCAT(EXTRACT(YEAR FROM  DATE (Month)),"年",EXTRACT(month FROM  DATE (Month)),"月") year_month,
-  contact_status,
-  forecast_phase,
+  datamart.fiscal_year(month) AS fiscal_year, --永続関数を参照
+  CONCAT(EXTRACT(YEAR FROM  DATE (month)),"年",EXTRACT(month FROM  DATE (month)),"月") year_month,
+  forecast_category,
   stage_label,
   office,
   department,
   user,
-  ifnull(amount,0) amount
+  ifnull(amount,0) AS amount
 FROM
   T1
 
@@ -87,10 +73,9 @@ UNION ALL
 
 SELECT
   month,
-  datamart.fiscal_year(Month) AS fiscal_year, --永続関数を参照
+  datamart.fiscal_year(month) AS fiscal_year, --永続関数を参照
   CONCAT(EXTRACT(YEAR FROM  DATE (Month)),"年",EXTRACT(month FROM  DATE (Month)),"月") year_month,
-  stage_name,
-  null forecast_phase,
+  null forecast_category,
   stage_label,
   office,
   department,
