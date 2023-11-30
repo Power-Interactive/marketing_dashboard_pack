17b2e8a354ad15b5349ff39feae904a6ffd634b8
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:42:28 +0900

    Update V_Forecast_Management_User.sql

--- SQL/datamart/V_Forecast_Management_User.sql
+++ SQL/datamart/V_Forecast_Management_User.sql
@@ -1,18 +1,15 @@
 WITH tmp0 AS (
-   SELECT 
-      DATE_TRUNC(close_date, MONTH) month
-   , CAST(SUM(amount) AS NUMERIC) as amount
-    ,department
-    ,office
-    ,CONCAT(last_name , first_name) name
-   FROM `pi-dashboard-398109.datawarehouse.Opportunity` t1
-   LEFT JOIN `pi-dashboard-398109.datawarehouse.User` t2
+   SELECT
+   DATE_TRUNC(t1.close_date, MONTH) month
+   ,t2.department AS department
+   ,t2.office AS office
+   ,CONCAT(t2.last_name , t2.first_name) name
+   ,CAST(SUM(t1.amount) AS NUMERIC) as amount
+   FROM `pi-dev-dashboard.datawarehouse.Opportunity` t1
+   LEFT JOIN `pi-dev-dashboard.datawarehouse.User` t2
     ON t1.owner_id = t2.id
    GROUP BY
-      DATE_TRUNC(close_date, MONTH) ,
-      Department,
-      Office,
-      CONCAT(last_name , first_name)
+      1,2,3,4
 ), 
 
 
@@ -20,75 +17,72 @@ MT
 AS
 (
 SELECT
-   month,
-   department,
-   office,
-   user,
-   sum(sales_target) sales_target,
-   sum(sales_result) sales_result
+   Month,
+   Department,
+   Office,
+   User,
+   sum(SalesTarget) SalesTarget,
+   sum(SalesResult) SalesResult
 FROM
-   `pi-dashboard-398109.datawarehouse.Sales_Target_User_Master`
+   `pi-dev-dashboard.master.SalesTarget_User`
 GROUP BY
-   month,
-   department,
-   office,
-   user
+   1,2,3,4
 ),
 
 tmp AS
 (
 SELECT
-   t1.month AS month,
+   t1.Month AS Month,
    --MasterのSalesResultを優先
   CASE
-    WHEN sum(tmp0.amount) IS NOT NULL AND sum(t1.sales_result) IS NOT NULL THEN sum(t1.sales_result)
-    WHEN sum(tmp0.amount) IS NULL AND sum(t1.sales_result) IS NOT NULL THEN sum(t1.sales_result)
-    WHEN sum(t1.sales_result) IS NULL THEN sum(tmp0.amount)
-  END amount,
-  t1.department,
-  t1.office,
-  t1.user AS name
+    WHEN sum(tmp0.Amount) IS NOT NULL AND sum(t1.SalesResult) IS NOT NULL THEN sum(t1.SalesResult)
+    WHEN sum(tmp0.Amount) IS NULL AND sum(t1.SalesResult) IS NOT NULL THEN sum(t1.SalesResult)
+    WHEN sum(t1.SalesResult) IS NULL THEN sum(tmp0.Amount)
+  END Amount,
+  t1.Department AS department,
+  t1.Office AS office,
+  t1.User AS Name
 FROM
    MT AS t1
 LEFT JOIN
    tmp0 AS tmp0
 ON
-   t1.month = tmp0.month
+   t1.Month = tmp0.Month
    AND
-   t1.department = tmp0.department
+   t1.Department = tmp0.Department
    AND
-   t1.office = tmp0.office
+   t1.Office = tmp0.office
    AND
-   t1.user = tmp0.name
+   t1.User = tmp0.Name
 GROUP BY
-   t1.month,
-   t1.department,
-   t1.office,
-   t1.user
+   t1.Month,
+   t1.Department,
+   t1.Office,
+   t1.User
 ),
 
 tmp2 AS (
    SELECT
        tmp1.*
-      ,tmp2.amount amount_1y
-      ,tmp1.amount/tmp2.amount amount_yoy
+      ,tmp2.Amount Amount_1y
+      ,tmp1.Amount/tmp2.Amount Amount_yoy
    FROM tmp tmp1
    LEFT JOIN tmp tmp2
-   ON DATE_SUB(tmp1.month, INTERVAL 1 year) = tmp2.month
-    AND tmp1.department = tmp2.department
-    AND tmp1.office = tmp2.office
-    AND tmp1.name = tmp2.name
+   ON DATE_SUB(tmp1.Month, INTERVAL 1 year) = tmp2.Month
+    AND tmp1.Department = tmp2.Department
+    AND tmp1.Office = tmp2.Office
+    AND tmp1.Name = tmp2.Name
 ),
 tmp3 AS (
    SELECT  
-       t1.month as month
-      ,t1.department AS department
-      ,t1.office AS office
+       t1.Month as month
+      ,t1.Department AS department
+      ,t1.Office AS office
       ,t1.user      
-      ,sum(t1.sales_target) as budget
-      ,sum(t2.amount) AS amount
-      ,sum(t2.amount_1y) AS amount_1y
-      ,sum(t2.amount_yoy) AS amount_yoy
+      ,sum(t1.SalesTarget) as budget
+      ,sum(t2.Amount) AS Amount
+      ,sum(t2.Amount_1y) AS Amount_1y
+      ,sum(t2.Amount_yoy) AS Amount_yoy
    FROM
       MT AS t1
    LEFT JOIN tmp2  t2
