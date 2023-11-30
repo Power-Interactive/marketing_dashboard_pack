95a7ffd88fc3102de0845accbbe34c54188de8ee
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:48:11 +0900

    Update and rename V_Opportunity_Last_Year.sql to V_Opportunity_LastYear.sql

--- SQL/datamart/V_Opportunity_LastYear.sql
+++ SQL/datamart/V_Opportunity_LastYear.sql
@@ -6,18 +6,11 @@ SELECT
   close_date AS closed_date,
   close_date AS month,
   CONCAT(EXTRACT(YEAR FROM  DATE (close_date)),"年",EXTRACT(month FROM  DATE (close_date)),"月") year_month,
+
   datamart.fiscal_year(close_date) AS fiscal_year,
-  /*
-  CASE
-    WHEN CAST(fiscal_year AS STRING) IN ('1') THEN CONCAT(1 + EXTRACT(YEAR FROM  DATE(close_date)),".Q1")
-    WHEN CAST(fiscal_year AS STRING) IN ('2') THEN CONCAT(EXTRACT(YEAR FROM  DATE(close_date)),".Q2")
-    WHEN CAST(fiscal_year AS STRING) IN ('3') THEN CONCAT(EXTRACT(YEAR FROM  DATE(close_date)),".Q3")
-    WHEN CAST(fiscal_year AS STRING) IN ('4') THEN CONCAT(EXTRACT(YEAR FROM  DATE(close_date)),".Q4")
-  END fiscal_year,
-  */
 
   date_sub(close_date, INTERVAL 1 Year) closed_date_lastyear,
-  detailded_opportunity_source AS lead_source, --本来はLeadSourceが入る
+  opportunity_source AS lead_source, --本来はLeadSourceが入る
   CASE
     WHEN forecast_phase IN ("Closed","Won") THEN 'Closed Won'
     WHEN forecast_phase IN ("Omitted","Lost") THEN 'Closed Lost'
@@ -25,14 +18,11 @@ SELECT
   END stage_name,
   COUNT(DISTINCT id) opportunity_count
 FROM
-  `pi-dashboard-398109.datawarehouse.Opportunity`
+  `pi-dev-dashboard.datawarehouse.Opportunity`
 WHERE
   forecast_phase in ('Closed','Omitted','Won','Lost')
 GROUP BY
-  close_date,
-  fiscal_year,
-  detailded_opportunity_source,
-  stage_name
+  1,2,3,4,5,6,7
 ),
 
 T2
@@ -46,7 +36,7 @@ FROM
 LEFT JOIN
   T1 AS S1
 ON
-  T1.Closed_date_lastyear = S1.Closed_date
+  T1.closed_date_lastyear = S1.closed_date
   AND
   T1.lead_source = S1.lead_source
   AND
