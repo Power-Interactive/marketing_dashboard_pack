0b483eecdbd1e4f489e5a3138d209ca68d0396ca
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:30:42 +0900

    Update V_Account_Contact.sql

--- SQL/datamart/V_Account_Contact.sql
+++ SQL/datamart/V_Account_Contact.sql
@@ -1,32 +1,32 @@
 SELECT
-  A.id account_id,
-  A.name account,
-  A.industry,
-  A.type,
-  A.amount,
+  A.Id AS account_id,
+  A.Name AS account,
+  A.industry AS industry,
+  A.type AS type,
+  A.amount AS amount,
   A.state,
   A.city,
   A.street,
-  A.end_of_period, --closing_monthから変更
+  A.fiscal_year AS end_of_period,
   A.website,
   A.rank,
   C.department,
   C.title,
-  C.last_name,
-  C.first_name,
-  C.id contactid,
+  C.last_name AS last_name,
+  C.first_name AS first_name,
+  C.id AS contactid,
   IFNULL(D.number_of_employee,'不明') AS number_of_employee
 FROM
   (SELECT * 
-    FROM `pi-dashboard-398109.datawarehouse.Account`
+    FROM `pi-dev-dashboard.datawarehouse.Account`
   ) A
 LEFT JOIN
     (
     SELECT * 
-    FROM `pi-dashboard-398109.datawarehouse.Contact`
+    FROM `pi-dev-dashboard.datawarehouse.Contact`
   ) C
 ON
-  A.Id = C.id
+  A.Id = C.account_id
 
 LEFT JOIN
   (
@@ -34,7 +34,7 @@ LEFT JOIN
     sfa_contactid,
     number_of_employee
   FROM
-    `pi-dashboard-398109.datawarehouse.Leads`
+    `pi-dev-dashboard.datawarehouse.Leads`
   ) D
 ON
   C.Id = D.sfa_contactid
