eb484b1df98f3778e085036cdc4ceb221f085d9c
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:42:15 +0900

    Update V_Forecast_Management_Team.sql

--- SQL/datamart/V_Forecast_Management_Team.sql
+++ SQL/datamart/V_Forecast_Management_Team.sql
@@ -3,10 +3,10 @@ WITH tmp0 AS (
    SELECT 
      DATE_TRUNC(t1.close_date, MONTH) month
     ,CAST(SUM(t1.amount) AS NUMERIC) as amount
-    ,t2.department AS department
+    ,t2.Department AS department
     ,t2.office AS office
-   FROM  `pi-dashboard-398109.datawarehouse.Opportunity` t1
-   LEFT JOIN `pi-dashboard-398109.datawarehouse.User` t2
+   FROM  `pi-dev-dashboard.datawarehouse.Opportunity` t1
+   LEFT JOIN `pi-dev-dashboard.datawarehouse.User` t2
       ON t1.owner_id = t2.id
    GROUP BY DATE_TRUNC(t1.close_date, MONTH) ,t2.department,t2.office
 ), 
@@ -16,17 +16,17 @@ MT
 AS
 (
 SELECT
-   month,
-   office,
-   department,
-   sum(sales_target) sales_target,
-   sum(sales_result) sales_result
+   Month,
+   Office,
+   Department,
+   sum(SalesTarget) SalesTarget,
+   sum(SalesResult) SalesResult
 FROM
-   `pi-dashboard-398109.datawarehouse.Sales_Target_Team_Master`
+   `pi-dev-dashboard.master.SalesTarget_Team_Type`
 GROUP BY
-   month,
-   office,
-   department
+   Month,
+   Office,
+   Department
 ),
 
 
@@ -34,61 +34,61 @@ GROUP BY
 tmp AS
 (
 SELECT
-   t1.month,
-   t1.department,
-   t1.office,
+   t1.Month AS Month,
+   t1.Department AS department,
+   t1.Office,
    --MasterのSalesResultを優先
   CASE
-    WHEN sum(tmp0.amount) IS NOT NULL AND sum(t1.sales_result) IS NOT NULL THEN sum(t1.sales_result)
-    WHEN sum(tmp0.amount) IS NULL AND sum(t1.sales_result) IS NOT NULL THEN sum(t1.sales_result)
-    WHEN sum(t1.sales_result) IS NULL THEN sum(tmp0.amount)
-  END amount
+    WHEN sum(tmp0.Amount) IS NOT NULL AND sum(t1.SalesResult) IS NOT NULL THEN sum(t1.SalesResult)
+    WHEN sum(tmp0.Amount) IS NULL AND sum(t1.SalesResult) IS NOT NULL THEN sum(t1.SalesResult)
+    WHEN sum(t1.SalesResult) IS NULL THEN sum(tmp0.Amount)
+  END Amount
 FROM
    MT AS t1
 LEFT JOIN
    tmp0 AS tmp0
 ON
-   t1.month = tmp0.month
+   t1.Month = tmp0.Month
    AND
-   t1.department = tmp0.department
+   t1.Department = tmp0.Department
    AND
-   t1.office = tmp0.office
+   t1.Office = tmp0.office
 GROUP BY
-   t1.month,
-   t1.department,
-   t1.office
+   t1.Month,
+   t1.Department,
+   t1.Office
 ),
 
 
 tmp2 AS (
    SELECT
-       tmp1.month
+       tmp1.Month
       ,tmp1.department
-      ,tmp1.office
-      ,tmp1.amount
-      ,tmp2.amount amount_1y
+      ,tmp1.Office
+      ,tmp1.Amount
+      ,tmp2.Amount Amount_1y
    FROM tmp tmp1
    LEFT JOIN tmp tmp2
-   ON DATE_SUB(tmp1.month, INTERVAL 1 year) = tmp2.month
-    AND tmp1.department = tmp2.department
+   ON DATE_SUB(tmp1.Month, INTERVAL 1 year) = tmp2.Month
+    AND tmp1.Department = tmp2.Department
     AND tmp1.office = tmp2.office
 ),
 
 tmp3 AS (
    SELECT  
-       t1.month
-      ,t1.department
-      ,t1.office
-      ,sum(t1.sales_target) as budget
-      ,sum(t2.amount) AS amount
-      ,sum(t2.amount_1y) AS amount_1y
-      ,sum(t2.amount)/sum(t2.amount_1y) AS amount_yoy
+       t1.Month as month
+      ,t1.Department AS department
+      ,t1.Office AS office
+      ,sum(t1.SalesTarget) as budget
+      ,sum(t2.Amount) AS Amount
+      ,sum(t2.Amount_1y) AS Amount_1y
+      ,sum(t2.Amount)/sum(t2.Amount_1y) AS Amount_yoy
    FROM 
       MT AS t1
    LEFT JOIN tmp2  t2
-      ON t1.month = t2.month
-        AND t1.office = t2.office
-        AND t1.department = t2.department
+      ON t1.month = t2.Month
+        AND t1.Office = t2.Office
+        AND t1.Department = t2.Department
    where t1.month is not null 
    GROUP BY
       t1.month,
@@ -105,4 +105,4 @@ UNPIVOT (
     amount
     FOR class IN (budget,amount,amount_1y,amount_yoy)
 )
-ORDER BY month
+ORDER BY Month
