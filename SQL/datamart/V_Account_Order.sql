9effa4e7537ecd85877627785cb2e39d861fb99e
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:39:14 +0900

    Update V_Account_Order.sql

--- SQL/datamart/V_Account_Order.sql
+++ SQL/datamart/V_Account_Order.sql
@@ -1,19 +1,19 @@
 SELECT
-  A.id account_id,
-  A.name account,
+  A.Id account_id,
+  A.Name account,
   A.rank,
-  O.fiscal_year,
+  O.fiscal_year AS fiscal_year,
   SUM(O.amount) amount
 FROM
   (
     SELECT * 
-    FROM `pi-dashboard-398109.datawarehouse.Account` 
+    FROM `pi-dev-dashboard.datawarehouse.Account` 
   ) A
 LEFT JOIN
   (
     SELECT * 
-    FROM `pi-dashboard-398109.datawarehouse.Opportunity` 
+    FROM `pi-dev-dashboard.datawarehouse.Opportunity`
   ) O
 ON
-  A.id = O.account_id
+  A.Id = O.account_id
 GROUP BY 1,2,3,4
