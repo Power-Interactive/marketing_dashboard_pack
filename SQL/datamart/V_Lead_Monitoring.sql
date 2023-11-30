8d2fab1cd1a59bb41612c3c5c67cace509fa681b
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:43:41 +0900

    Update V_Lead_Monitoring.sql

--- SQL/datamart/V_Lead_Monitoring.sql
+++ SQL/datamart/V_Lead_Monitoring.sql
@@ -5,7 +5,7 @@ AS
 (SELECT 
     FORMAT_DATE("%F", date_trunc(created_datetime,Month) ) month
     ,COUNT(distinct id) cnt
-FROM `pi-dashboard-398109.datawarehouse.Leads` 
+FROM `pi-dev-dashboard.datawarehouse.Leads` 
 GROUP BY FORMAT_DATE("%F", date_trunc(created_datetime,Month) )
 ),
 
@@ -16,7 +16,7 @@ AS
 SELECT 
     FORMAT_DATE("%F", date_trunc(created_datetime,Month) ) month
     ,COUNT(distinct id)  cnt
-FROM `pi-dashboard-398109.datawarehouse.Opportunity` --SFA.OppotunitySnapshot_*から変更
+FROM `pi-dev-dashboard.datawarehouse.Opportunity` --SFA.OppotunitySnapshot_*から変更
 GROUP BY FORMAT_DATE("%F", date_trunc(created_datetime,Month) )
 ),
 
@@ -26,7 +26,7 @@ AS (
     SELECT month,closedwon,closedlost
     FROM (
         SELECT 
-        FORMAT_DATE("%F", date_trunc(created_datetime,Month) ) Month
+        FORMAT_DATE("%F", date_trunc(close_date,Month) ) month
         ,CASE forecast_phase
                 WHEN 'Closed' THEN 'ClosedWon'
                 WHEN 'Won' THEN 'ClosedWon' --サンプルデータ修正用
@@ -34,10 +34,11 @@ AS (
                 WHEN 'Lost' THEN 'ClosedLost' --サンプルデータ修正用
                 ELSE '-'
             END AS stage_name
-        ,count(distinct id) cnt_order
-        FROM  `pi-dashboard-398109.datawarehouse.Opportunity`  t2   --SFA.OppotunitySnapshot_*から変更
-        WHERE forecast_phase in ('Closed','Omitted','Won','Lost') --Closed:受注, Omitted:ロスト
-        GROUP BY FORMAT_DATE("%F", date_trunc(created_datetime,Month) ),stage_name
+        ,count(distinct id) Cnt_Order
+        FROM  `pi-dev-dashboard.datawarehouse.Opportunity`  t2   --SFA.OppotunitySnapshot_*から変更
+        WHERE
+            forecast_phase in ('Closed','Omitted','Won','Lost') --Closed:受注, Omitted:ロスト
+        GROUP BY FORMAT_DATE("%F", date_trunc(close_date,Month) ),stage_name
     )
     PIVOT(
         SUM(Cnt_Order)
@@ -52,7 +53,7 @@ SELECT
     ,OPP.cnt new_opportunity
     ,OPP_LOST_WON.closedwon
     ,OPP_LOST_WON.closedlost
-FROM `pi-dashboard-398109.dl_master.Calender_Master` cal
+FROM `pi-dev-dashboard.master.calender` cal
 LEFT JOIN L ON L.month = CAST(cal.month AS string)
 LEFT JOIN OPP ON OPP.month = CAST(cal.month AS string)
 LEFT JOIN OPP_LOST_WON ON OPP_LOST_WON.month = CAST(cal.month AS string)
