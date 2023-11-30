695be7da8991593d3a43991976cdfcad80effbf3
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:41:57 +0900

    Update V_Forecast_Management.sql

--- SQL/datamart/V_Forecast_Management.sql
+++ SQL/datamart/V_Forecast_Management.sql
@@ -2,7 +2,7 @@ WITH tmp0 AS (
    SELECT 
       DATE_TRUNC(close_date, MONTH) close_date
    , CAST(SUM(amount) AS NUMERIC) as amount
-   FROM `pi-dashboard-398109.datawarehouse.Opportunity`
+   FROM `pi-dev-dashboard.datawarehouse.Opportunity`
    GROUP BY DATE_TRUNC(close_date, MONTH) 
    
 ), 
@@ -11,41 +11,41 @@ MT
 AS
 (
 SELECT
-   month,
-   sum(sales_target) sales_target,
-   sum(sales_result) sales_result
+   Month,
+   sum(SalesTarget) SalesTarget,
+   sum(SalesResult) SalesResult
 FROM
-   `pi-dashboard-398109.datawarehouse.Sales_Target_Team_Master`
+   `pi-dev-dashboard.master.SalesTarget_Team_Type`
 GROUP BY
-   month
+   Month
 ),
 
 tmp AS
 (
 SELECT
-   t1.month AS close_date,
+   t1.Month AS close_date,
    --MasterのSalesResultを優先
   CASE
-    WHEN sum(tmp0.amount) IS NOT NULL AND sum(t1.sales_result) IS NOT NULL THEN sum(t1.sales_result)
-    WHEN sum(tmp0.amount) IS NULL AND sum(t1.sales_result) IS NOT NULL THEN sum(t1.sales_result)
-    WHEN sum(t1.sales_result) IS NULL THEN sum(tmp0.amount)
-  END amount
+    WHEN sum(tmp0.Amount) IS NOT NULL AND sum(t1.SalesResult) IS NOT NULL THEN sum(t1.SalesResult)
+    WHEN sum(tmp0.Amount) IS NULL AND sum(t1.SalesResult) IS NOT NULL THEN sum(t1.SalesResult)
+    WHEN sum(t1.SalesResult) IS NULL THEN sum(tmp0.Amount)
+  END Amount
 FROM
    MT AS t1
 LEFT JOIN
    tmp0 AS tmp0
 ON
-   t1.month = tmp0.close_date
+   t1.Month = tmp0.close_date
 GROUP BY
-   t1.month
+   t1.Month
 ),
 
 tmp2 AS (
    SELECT
       tmp1.close_date
-      ,tmp1.amount
-      ,tmp2.amount amount_1y
-      ,tmp1.amount/tmp2.amount amount_yoy
+      ,tmp1.Amount
+      ,tmp2.Amount Amount_1y
+      ,tmp1.Amount/tmp2.Amount Amount_yoy
    FROM tmp tmp1
    LEFT JOIN tmp tmp2
    ON DATE_SUB(tmp1.close_Date, INTERVAL 1 year) = tmp2.close_Date
@@ -54,14 +54,14 @@ tmp2 AS (
 tmp3 AS (
    SELECT  
       t1.month as close_date
-      ,sum(t1.sales_target) as budget
-      ,sum(t2.amount) AS amount
-      ,sum(t2.amount_1y) AS amount_1y
-      ,sum(t2.amount_yoy) AS amount_yoy
+      ,sum(t1.SalesTarget) as budget
+      ,sum(t2.Amount) AS Amount
+      ,sum(t2.Amount_1y) AS Amount_1y
+      ,sum(t2.Amount_yoy) AS Amount_yoy
    FROM
       MT AS t1
    LEFT JOIN tmp2  t2
-      ON t1.month = t2.close_date
+      ON t1.month = t2.Close_Date
    where t1.month is not null 
    GROUP BY
       t1.month
@@ -71,10 +71,10 @@ tmp3 AS (
 -- UNPIVOT：横持ちを縦持ちに変換
 SELECT
  *
- ,CONCAT(EXTRACT(YEAR FROM  DATE (Close_Date)),"年",EXTRACT(month FROM  DATE (Close_Date)),"月") year_month,
+ ,CONCAT(EXTRACT(YEAR FROM  DATE (close_date)),"年",EXTRACT(month FROM  DATE (close_date)),"月") year_month,
 FROM tmp3
 UNPIVOT (
     amount
     FOR class IN (budget,amount,amount_1y,amount_yoy)
 )
-ORDER BY close_date
+ORDER BY Close_Date
