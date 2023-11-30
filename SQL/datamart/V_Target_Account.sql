4cdb6be8b5b20faf80b182cab07887d5489ca5dc
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:50:38 +0900

    Update V_Target_Account.sql

--- SQL/datamart/V_Target_Account.sql
+++ SQL/datamart/V_Target_Account.sql
@@ -1,10 +1,13 @@
 SELECT 
-L.id
-,L.sfa_accountid 
-,datamart.lifecycle_status(L.lifecycle_status) AS lifecycle_status --永続関数を参照
-,A.industry
-,A.state
-,A.rank
-FROM `pi-dashboard-398109.datawarehouse.Leads`  L 
-LEFT JOIN `pi-dashboard-398109.datawarehouse.Account` A 
-ON L.sfa_accountid  = A.id
+  L.id AS id,
+  L.sfa_accountid AS sfa_accountid,
+  datamart.lifecycle_status(L.lifecycle_status) AS lifecycle_status, --永続関数を参照
+  A.industry AS industry,
+  A.state AS state,
+  A.rank AS rank
+FROM
+  `pi-dev-dashboard.datawarehouse.Leads`  L 
+LEFT JOIN
+  `pi-dev-dashboard.datawarehouse.Account` A 
+ON
+  L.sfa_accountid = A.id
