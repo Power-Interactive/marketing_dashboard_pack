81dbcfbc63778cfbc4a1a8064db96dc543c08ed4
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2023-11-30 14:39:30 +0900

    Update V_Channel_KPI.sql

--- SQL/datamart/V_Channel_KPI.sql
+++ SQL/datamart/V_Channel_KPI.sql
@@ -1,10 +1,10 @@
 SELECT
-  month,
-  month AS date,
+  Month AS month,
+  Month AS date,
   CONCAT(EXTRACT(YEAR FROM  DATE (Month)),"年",EXTRACT(month FROM  DATE (Month)),"月") year_month,
   datamart.fiscal_year(Month) AS fiscal_year, --永続関数を参照
-  channel AS lead_source,
-  AfterStatus as after_status,
-  kpi
+  Channel AS lead_source,
+  AfterStatus AS after_status,
+  KPI AS kpi
 FROM
-  `pi-dashboard-398109.datawarehouse.Channel_KPI_Master`
+  `pi-dev-dashboard.master.ChannelKPI`
