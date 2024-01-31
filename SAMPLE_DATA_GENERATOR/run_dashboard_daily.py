b7427f74a8572eb6f13b3811405531e9a6529c6b
OSHIMA Hisaaki <duvel2005@gmail.com>
2024-01-31 16:15:51 +0900

    世代管理追加 エラーのリカバリーのために各csvファイルおよびconf_dashboard.jsonファイルを三世代にわたってバックアップするように変更 sample/_1...一世代前 sample/_2...二世代前 sample/_3...三世代前

--- SAMPLE_DATA_GENERATOR/run_dashboard_daily.py
+++ SAMPLE_DATA_GENERATOR/run_dashboard_daily.py
@@ -1,16 +1,48 @@
 #!/usr/bin/python3
 
 import subprocess
-import sys
-import re
-import os
 import datetime
 from zoneinfo import ZoneInfo
 
+list_FN_backup =   ["Activities_ChangeDataValue_New.csv",
+                    "Activities_ChangeDataValue.csv",
+                    "Activities_ClickEmail_New.csv",
+                    "Activities_ClickEmail.csv",
+                    "Activities_ClickLink_New.csv",
+                    "Activities_ClickLink.csv",
+                    "Activities_FillOutForm_New.csv",
+                    "Activities_FillOutForm.csv",
+                    "Activities_OpenEmail_New.csv",
+                    "Activities_OpenEmail.csv",
+                    "Activities_VisitWebpage_New.csv",
+                    "Activities_VisitWebpage.csv",
+                    "Leads.csv",
+                    "Leads_SQL.csv",
+                    "Opportunity.csv",
+                    "OpportunityHistory.csv",
+                    "OpportunityHistory_New.csv"]
+
+
 dt_NOW = datetime.datetime.now(ZoneInfo("Asia/Tokyo"))
 
+baseCmd = "cp sample/_2/{} sample/_3/{}"
+for fn in list_FN_backup:
+    executeCmd = baseCmd.format(fn, fn)
+    result = subprocess.run(executeCmd, shell=True)
 
+baseCmd = "cp sample/_1/{} sample/_2/{}"
+for fn in list_FN_backup:
+    executeCmd = baseCmd.format(fn, fn)
+    result = subprocess.run(executeCmd, shell=True)
 
+baseCmd = "cp sample/{} sample/_1/{}"
+for fn in list_FN_backup:
+    executeCmd = baseCmd.format(fn, fn)
+    result = subprocess.run(executeCmd, shell=True)
+
+result = subprocess.run("cp sample/_2/conf_dashboard.json sample/_3", shell=True)
+result = subprocess.run("cp sample/_1/conf_dashboard.json sample/_2", shell=True)
+result = subprocess.run("cp conf_dashboard.json sample/_1", shell=True)
 
 baseCmd = "python3 run_dashboard.py start=1 count=1 > logs/RD_{:%Y%m%d-%H%M%S}.log 2> logs/RD_{:%Y%m%d-%H%M%S}.err"
 
@@ -22,24 +54,9 @@ result = subprocess.run(executeCmd, shell=True)
 
 
 baseCmd = "cp sample/{} /opt/sync/tmp/sample/{}"
-list_FN = ["Activities_ChangeDataValue_New.csv",
-           "Activities_ChangeDataValue.csv",
-           "Activities_ClickEmail_New.csv",
-           "Activities_ClickEmail.csv",
-           "Activities_ClickLink_New.csv",
-           "Activities_ClickLink.csv",
-           "Activities_FillOutForm_New.csv",
-           "Activities_FillOutForm.csv",
-           "Activities_OpenEmail_New.csv",
-           "Activities_OpenEmail.csv",
-           "Activities_VisitWebpage_New.csv",
-           "Activities_VisitWebpage.csv",
-           "Leads.csv",
-           "Opportunity.csv",
-           "OpportunityHistory.csv",
-           "OpportunityHistory_New.csv"]
+list_FN_sync = [s for s in list_FN_backup if s != 'Leads_SQL.csv']
 
-for fn in list_FN:
+for fn in list_FN_sync:
     executeCmd = baseCmd.format(fn, fn)
     result = subprocess.run(executeCmd, shell=True)
 
