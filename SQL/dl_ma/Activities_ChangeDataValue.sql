7db10bdfc1c730396ce689102a34d88bbe95ea6c
PIKosukeYagi <128957428+PIKosukeYagi@users.noreply.github.com>
2024-02-05 17:01:36 +0900

    Update Activities_ChangeDataValue.sql

--- SQL/dl_ma/Activities_ChangeDataValue.sql
+++ SQL/dl_ma/Activities_ChangeDataValue.sql
@@ -1,13 +1,12 @@
-CREATE TABLE							
-	dl_ma.Activities_ChangeDataValue
-(							
-	ActivityId			STRING,
-	ActivityDate		TIMESTAMP,
-	AttributeName		INT64,
-	AttributeNameValue	STRING,
-	CampaignId			STRING,
-	LeadId				STRING,
-	NewValue			STRING,
-	OldValue			STRING,
-	Reason				STRING
-)	
+CREATE TABLE dejimapro.dl_ma.Activities_ChangeDataValue
+(
+ActivityId STRING,
+ActivityDate TIMESTAMP,
+AttributeName INTEGER,
+AttributeNameValue STRING,
+CampaignId STRING,
+LeadId STRING,
+NewValue STRING,
+OldValue STRING,
+Reason STRING,
+)
